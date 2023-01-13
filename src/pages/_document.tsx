6bef754ecee59cc6041d15866a0e3562d1ef1afa
End313234 <end313234@protonmail.com>
2023-01-13 13:56:05 -0300

    feat: set style sheet and typography

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const Document = () => {
+	return (
+		<Html>
+			<Head>
+				<title>Weather App</title>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+				<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600;700&family=Raleway:wght@400;500;600;700&display=swap" rel="stylesheet" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
+}
+
+export default Document;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
 	content: ["./src/**/*.tsx"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bluepurple: {
+					300: "#585676",
+					500: "#1E213A",
+					800: "#100E1D",
+				},
+				yellow: {
+					300: "#FFEC65",
+				},
+				zinc: {
+					200: "#E7E7EB",
+				},
+			},
+			fontFamily: {
+				sans: "Raleway, sans-serif",
+				credits: "Montserrat, sans-serif",
+			},
+		},
 	},
 	plugins: [],
 };
