9a3682c56a6318ce1840009d1554a57106f30f19
End313234 <end313234@protonmail.com>
2023-01-13 22:10:42 -0300

    feat: improve the selector for temperature scale

--- src/web/components/temperature-scale-selector/index.tsx
+++ src/web/components/temperature-scale-selector/index.tsx
@@ -1,8 +1,28 @@
-export const TemperatureScaleSelector = () => {
+import { Dispatch, FC, SetStateAction } from "react";
+
+interface TemperatureScaleSelectorProps {
+	scale: number;
+	toggleScale: Dispatch<SetStateAction<number>>;
+}
+
+export const TemperatureScaleSelector: FC<TemperatureScaleSelectorProps> = ({ scale, toggleScale }) => {
+	const toggler = (id: number) => () => {
+		toggleScale(id)
+	}
 	return (
 		<div className="flex gap-4">
-			<button className="rounded-full px-4 h-fit">°C</button>
-			<button className="rounded-full px-4 h-fit">°F</button>
+			<button
+				onClick={toggler(0)}
+				className={`rounded-full px-4 h-fit font-bold ${scale ? "bg-blurple-500" : "bg-zinc-200 text-bluepurple-500"}`}
+			>
+				°C
+			</button>
+			<button
+				onClick={toggler(1)}
+				className={`rounded-full px-4 h-fit font-bold ${scale ? "bg-zinc-200 text-bluepurple-500" : "bg-blurple-500"}`}
+			>
+				°F
+			</button>
 		</div>
 	)
 };
--- src/web/pages/home/index.tsx
+++ src/web/pages/home/index.tsx
@@ -1,15 +1,18 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { FutureWeatherCard } from "web/components/future-weather-card";
 import { TemperatureScaleSelector } from "web/components/temperature-scale-selector";
 import { WeatherStats } from "./weather-stats";
 
 export const Home: FC = () => {
+	// 0 for Celsius and 1 for Fahrenheit
+	const [temperatureScale, setTemperatureScale] = useState(0);
+
 	return (
 		<main className="flex">
 			<WeatherStats />
 			<div className="mt-5 flex flex-col items-center px-10">
 				<div className="flex items-end justify-end w-full">
-					<TemperatureScaleSelector />
+					<TemperatureScaleSelector scale={temperatureScale} toggleScale={setTemperatureScale} />
 				</div>
 				<div className="flex gap-4 mt-10">
 					<FutureWeatherCard />
